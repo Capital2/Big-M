8b718ea878f66230323ebf3aa9b4ff7be9ff3d89
BlueGob <59932913+BlueGob@users.noreply.github.com>
2022-11-07 12:15:55 +0100

    add user validator input

---
+++ backend/validator.py
@@ -0,0 +1,26 @@
+import re
+input ="3 3 x + 2 y> = 5 "
+input = "Min Z = 500x+200y+500z"
+def validateInput(input):
+    
+    #remove white spaces from the input string
+    strippedInput =""
+    for i in input:
+        if(i !=" "):
+            strippedInput = strippedInput + i
+    
+    #match regEx for constraints
+    matchConstraint = re.findall("^[0-9]+[x|X][+][0-9]+[y|Y]([+][0-9]+[z|Z]){0,1}[<|>]{0,1}[=]{0,1}[0-9]+$",strippedInput)
+    if(len(matchConstraint)!=0):
+        return True
+    
+     #match regEx for gain
+    matchGain = re.findall("^(Max|max|Min|min)[Z|z|W|w][=][0-9]+[x|X][+][0-9]+[y|Y]([+][0-9]+[z|Z]){0,1}",strippedInput)
+    if(len(matchGain)!=0):
+        return True
+   
+    return False
+
+
+
+print(validateInput(input))
