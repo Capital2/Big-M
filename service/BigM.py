c38198913f3f7d6a06fe3cf76ceff154375ac53d
Fedi Takeli <fedi.takeli1@gmail.com>
2022-11-07 15:06:42 +0100

    added interfaces for tested funcs in BigM.py

--- service/BigM.py
+++ service/BigM.py
@@ -1,2 +1,25 @@
 class BigM:
-    pass
+
+    def validateUserInput(userInput: str) -> bool:
+        """
+        Checks whether the input provided by the user is well formatted or not
+        Arguments:
+            userInput : String
+        Returns:
+            True if :
+                - User input follows the pattern: nx + ny + nz ( >= | <= | > | < | = ) n 
+               Or
+                - User input follows the pattern: (Max | Min) Z = nx + ny + nz
+            False otherwise
+        """
+        pass
+
+    def formatUserInput(userInput: list[str]) -> list[list[int]]:
+        """
+        The function takes an array of strings representing the input taken and **validated** from a jupyter user.
+        The function returns a matrix.
+        Arguments:
+            userInput : array of string
+        Returns:
+            Matrix following the format shown in the output image bellow
+        """
