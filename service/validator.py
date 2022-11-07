65aeb697a7fa1c261e2a00684ac8a6cefc6dd1b7
BlueGob <59932913+BlueGob@users.noreply.github.com>
2022-11-07 16:01:21 +0100

    fixed bug in regex

--- service/validator.py
+++ service/validator.py
@@ -1,4 +1,5 @@
 import re
+input = "MaX Z = 3X+Y+80z"
 def ValidateUserInput(input):
     '''
     Validate user input, check if the objectif or the constraint is valid 
@@ -9,22 +10,24 @@ def ValidateUserInput(input):
         if(i !=" "):
             strippedInput = strippedInput + i
     
+    strippedInput = strippedInput.lower()
+    
     #get occurence of each variable
-    occurenceOfX =len(re.findall("x|X",strippedInput))
-    occurenceOfY = len(re.findall("y|Y",strippedInput))
-    occurenceOfZ = len(re.findall("z|Z",strippedInput))
+    occurenceOfX =len(re.findall("[^a-zA-Z]x",strippedInput))
+    occurenceOfY = len(re.findall("y",strippedInput))
+    occurenceOfZ = len(re.findall("[^a-zA-Z]z",strippedInput))
 
     #check if each variable exist once at most
     if(occurenceOfX > 1 or occurenceOfY > 1 or occurenceOfZ > 1):
         return False
    
     #match regEx for constraints
-    matchConstraint = re.findall("^[-]{0,1}[0-9]*[x|X|y|Y|z|Z]([+|-][0-9]*[x|X|y|Y|z|Z])*[<|>]{0,1}[=]{0,1}[0-9]+$",strippedInput)
+    matchConstraint = re.findall("^[-]{0,1}[0-9]*[x|y|z|]([+|-][0-9]*[x|y|z])*[<|>]{0,1}[=]{0,1}[0-9]+$",strippedInput)
     if(len(matchConstraint)!=0):
         return True
     
      #match regEx for gain
-    matchGain = re.findall("^(Max|max|Min|min)[Z|z|W|w][=][-]{0,1}[0-9]*[x|X][+|-][0-9]*[y|Y]([+|-][0-9]*[z|Z]){0,1}$",strippedInput)
+    matchGain = re.findall("^(max|min)[a-zA-Z][=][-]{0,1}[0-9]*[x][+|-][0-9]*[y]([+|-][0-9]*[z]){0,1}$",strippedInput)
     if(len(matchGain)!=0):
         return True
    
