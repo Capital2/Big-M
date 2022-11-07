e79543deba1cc4fb823a62c444151c01585cd2f8
BlueGob <59932913+BlueGob@users.noreply.github.com>
2022-11-07 12:24:03 +0100

    polishing code

--- backend/validator.py
+++ backend/validator.py
@@ -1,8 +1,6 @@
 import re
-input ="3 3 x + 2 y> = 5 "
-input = "Min Z = 500x+200y+500z"
 def validateInput(input):
-    
+
     #remove white spaces from the input string
     strippedInput =""
     for i in input:
@@ -10,17 +8,13 @@ def validateInput(input):
             strippedInput = strippedInput + i
     
     #match regEx for constraints
-    matchConstraint = re.findall("^[0-9]+[x|X][+][0-9]+[y|Y]([+][0-9]+[z|Z]){0,1}[<|>]{0,1}[=]{0,1}[0-9]+$",strippedInput)
+    matchConstraint = re.findall("^[-]{0,1}[0-9]+[x|X][+|-][0-9]+[y|Y]([+][0-9]+[z|Z]){0,1}[<|>]{0,1}[=]{0,1}[0-9]+$",strippedInput)
     if(len(matchConstraint)!=0):
         return True
     
      #match regEx for gain
-    matchGain = re.findall("^(Max|max|Min|min)[Z|z|W|w][=][0-9]+[x|X][+][0-9]+[y|Y]([+][0-9]+[z|Z]){0,1}",strippedInput)
+    matchGain = re.findall("^(Max|max|Min|min)[Z|z|W|w][=][-]{0,1}[0-9]+[x|X][+|-][0-9]+[y|Y]([+|-][0-9]+[z|Z]){0,1}",strippedInput)
     if(len(matchGain)!=0):
         return True
    
     return False
-
-
-
-print(validateInput(input))
