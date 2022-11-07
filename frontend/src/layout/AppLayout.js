7a1a62333e40b41b5de519b184db8116c405ffd3
MohamedAmineBenAmmar <ma.benammar5@gmail.com>
2022-11-07 05:31:25 +0100

    seting up the basic folder structure

---
+++ frontend/src/layout/AppLayout.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AppLayout() {
+  return (
+    <div>AppLayout</div>
+  )
+}
---
+++ frontend/src/layout/SideNav.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SideNav() {
+  return (
+    <div>SideNav</div>
+  )
+}
