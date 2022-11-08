b8d213966a5be2aea1e88e16f2904f247dd21a16
Fedi Takeli <fedi.takeli1@gmail.com>
2022-11-08 22:17:29 +0100

    updated test_validateUserInput

--- tests/BigM_test.py
+++ tests/BigM_test.py
@@ -4,20 +4,23 @@ import unittest
 import service.BigM as bm
 
 class TestBigM(unittest.TestCase):
-    exampleConsTrue = ["3x+y=1", "x-Y>=0", " x +  50y<6 ", "y<5", "   x+y-z>0 "]
-    exampleObjectiveTrue = ["Max Z = 3x+y", "mIn z= 2x-y", " Min z =3x +50y - z    ", "maX Z = x+y", "MAX z = 50x+2000y", "Max j = 3x + y"]
 
-    exampleConsFalse = ["3x>==1", "x +x +y< 1", "x * y > 3", "ax + 5b < 2", "x + y >3z", "x>z", "x +>2"]
-    exampleObjectiveFalse = [" z = 3x+y ", "m z = 3x+y", " = 3x + y", "Max Z = 3x +"]
+    def setUp(self) -> None:
+        self.exampleConsTrue = ["3x+y=1", "x-Y>=0", " x +  50y<6 ", "   x+y-z>0 "]
+        self.exampleObjectiveTrue = ["Max Z = 3x+y", "mIn z= 2x-y", " Min j =3x +50y - z    ", "maX w = x+y", "MAX z = 50x+2000y", "Max j = 3x + y"]
+
+        self.exampleConsFalse = ["3x>==1", "x +x +y< 1", "x * y > 3", "ax + 5b < 2", "x + y >3z", "x>z", "x +>2", "y<0"]
+        self.exampleObjectiveFalse = [" z = 3x+y ", "m z = 3x+y", " = 3x + y", "Max Z = 3x +"]
+        return super().setUp()
 
     def test_validateUserInput(self):
         for t in (self.exampleObjectiveTrue + self.exampleConsTrue):
             tested = bm.BigM.validateUserInput(t)
-            self.assertTrue(tested)
+            self.assertTrue(tested,f"{t} should be true")
 
         for t in (self.exampleObjectiveFalse + self.exampleConsFalse):
             tested = bm.BigM.validateUserInput(t)
-            self.assertFalse(tested)
+            self.assertFalse(tested,f"{t} should be false")
 
 if __name__ == '__main__':
     unittest.main()
