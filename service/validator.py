5fb66b6fc9ca10693c889d53446cdce33713ad64
BlueGob <moslemgannoun@gmail.com>
2022-11-09 21:42:51 +0100

    fixed bug

--- service/validator.py
+++ service/validator.py
@@ -5,11 +5,12 @@ def ValidateUserInput(input):
     '''
     #remove white spaces from the input string
     strippedInput = re.sub(' ','', input.lower())
+
     print(strippedInput)
     #get occurence of each variable
-    occurenceOfX =len(re.findall("x[^a-z]",strippedInput))
-    occurenceOfY = len(re.findall("y",strippedInput))
-    occurenceOfZ = len(re.findall("[^a-z]z",strippedInput))
+    occurenceOfX =re.sub('(max|min)[a-z]','',strippedInput).count("x")
+    occurenceOfY=re.sub('(max|min)[a-z]','',strippedInput).count("y")
+    occurenceOfZ =re.sub('(max|min)[a-z]','',strippedInput).count("z")
 
     #check if each variable exist once at most
     if(occurenceOfX > 1 or occurenceOfY > 1 or occurenceOfZ > 1):
@@ -21,7 +22,7 @@ def ValidateUserInput(input):
         return True
     
      #match regEx for gain
-    matchGain = re.findall("^(max|min)[a-zA-Z][=][-]{0,1}[0-9]*[x][+|-][0-9]*[y]([+|-][0-9]*[z]){0,1}$",strippedInput)
+    matchGain = re.findall("^(max|min)[a-zA-Z][=][-]{0,1}[0-9]*[x|y|z][+|-][0-9]*[x|z|y]([+|-][0-9]*[x|y|z]){0,1}$",strippedInput)
     if(len(matchGain)!=0):
         return True
    
