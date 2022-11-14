b18f81f4892b2d3cef8db9998bbda7309856f588
MohamedAmineBenAmmar <ma.benammar5@gmail.com>
2022-11-14 16:02:50 +0100

    advancing

--- service/utilities.py
+++ service/utilities.py
@@ -1,2 +1,11 @@
+import numpy as np;
+
 class Utilities:
-    pass
+    @staticmethod
+    def get_col_index(matrix: np.matrix, i: int) -> int:
+        ix = -1
+        for j in range(0, matrix.shape[1]):
+            if matrix.item(i, j) != 0:
+                ix = j
+                break
+        return ix
