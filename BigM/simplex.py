a9e02755b5f5e8e6b25735174b47003c37a302b5
Fedi Takeli <fedi.takeli1@gmail.com>
2022-11-17 21:31:10 +0100

    still merging

--- BigM/simplex.py
+++ BigM/simplex.py
@@ -6,14 +6,32 @@ from utilities import Utilities
 
 class Simplex():
 
-    # Under test
-    def __select_vars(self, init_simplex_df: pd.DataFrame) -> Variables:
-        pass
+    def __select_vars(self, init_simplex_df: pd.DataFrame) -> tuple[Variables, Variables]:
+        """
+        Selects the basic and non-basic variables from the initial simplex dataframe.
+        """
+        # TODO: what if 2 variables have the only non-zero value in the same row? which one to select?
+        ret_vars = ([], [])
+        for col in init_simplex_df.columns:
+            if col in ['condition', 'p']: # skip the condition and p columns
+                continue
+            column = init_simplex_df.loc[:, col]
+            if column[column != 0].count() == 1: # basic variable (i.e. have only 1 non zero value)
+                nonZeroRow = column[column != 0].index[0] # get the row index of the non-zero value
+                ret_vars[0].append((col, init_simplex_df.loc[nonZeroRow]['condition'], nonZeroRow))
+            else: # non basic variable
+                ret_vars[1].append((col, 0, -1))
+        return ret_vars
 
-    # Under test
     def __verify_solution_existence(self, variables: Variables) -> bool:
-        exist: bool = True
-        return exist
+        """
+        Verifies if the solution exists.
+        checks if all the basic variables are positive.
+        """
+        for _, value, _ in variables[0]:
+            if value < 0:
+                return False
+        return True
 
     def __find_equation_coef(self, iteration: pd.DataFrame) -> dict:
         equation: pd.Series = iteration.iloc[-1]
