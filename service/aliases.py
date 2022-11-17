09bf614fbe618063495f4a1fd9c6d572e40862e2
MohamedAmineBenAmmar <ma.benammar5@gmail.com>
2022-11-17 08:28:29 +0100

    finished run big M

--- service/aliases.py
+++ service/aliases.py
@@ -1,15 +1,17 @@
 import pandas as pd
 
-Variables = dict[str, float | int]
-# example: {"name": "x1": "value": 3.0}
+Variable = tuple[str, int | float, int]
+# example: ("x1", "5.0", 2) x1 with value of 5.0 belongs to the second equation
+
+Variables = list[Variable]
+# example: [("x1", "5.0", 2), ("x1", "5.0", 2)]
 
 Iteration = tuple[pd.DataFrame, Variables, Variables]
-# example: (DataFrame, {"name": "x1": "value": 3.0}, {"name": "s1": "value": 0})
-# example: (simplex_df, basic_vars, non_basic_vars)
+# example: (DataFrame, [("x1", "5.0", 2), ("x1", "5.0", 2)], [("s1", "0", 1), ("s2", "5.0", 4)])
 
 Iterations = list[Iteration]
 # example: [
-# (DataFrame, {"name": "x1": "value": 3.0}, {"name": "s1": "value": 0}),
-# (DataFrame, {"name": "x1": "value": 3.0}, {"name": "s1": "value": 0})
+# (DataFrame, [("x1", "5.0", 2), ("x1", "5.0", 2)], [("s1", "0", 1), ("s2", "5.0", 4)]),
+# (DataFrame, [("x1", "5.0", 2), ("x1", "5.0", 2)], [("s1", "0", 1), ("s2", "5.0", 4)]),
 # ...
 # ]
