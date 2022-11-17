088033c22cccc2ad5a25678d518bfb17011c32ba
MohamedAmineBenAmmar <ma.benammar5@gmail.com>
2022-11-17 08:28:29 +0100

    finished run big M

--- service/main.py
+++ service/main.py
@@ -1,9 +1,26 @@
 import numpy as np
 from BigM import BigM
 
+def display_iteration(iteration):
+    print("==============================================================================================================")
+    print(iteration[0])
+    print()
+    print("basic variables")
+    print(iteration[1])
+    print("non basic variables")
+    print(iteration[2])
+    print()
+    print("==============================================================================================================")
 
-fake_data = np.matrix([[1, 1, 0, 1], [1, 0, 1, -1], [0, 1, 1, 3],[20, 5, 10, 0], [-2, 0, 2, 0]]) 
-big_m = BigM()
+
+
+def main():
+    fake_data = np.matrix([[1, 1, 0, 1], [1, 0, 1, -1], [0, 1, 1, 3],[20, 5, 10, 0], [-2, 0, 2, 0]]) 
+    big_m = BigM()
+    iterations = big_m.runBigM(fake_data)
+    for iteration in iterations:
+        display_iteration(iteration)
+
 
 if __name__ == "__main__":
-    big_m.runBigM(fake_data)
+    main()
--- service/simplex.py
+++ service/simplex.py
@@ -1,14 +1,16 @@
 import numpy as np
 import pandas as pd
-from aliases import Iterations, Variables
+from aliases import Iterations, Variables, Variable
 from utilities import Utilities
 
 
 class Simplex():
 
+    # Under test
     def __select_vars(self, init_simplex_df: pd.DataFrame) -> Variables:
         pass
 
+    # Under test
     def __verify_solution_existence(self, variables: Variables) -> bool:
         exist: bool = True
         return exist
@@ -20,8 +22,6 @@ class Simplex():
 
         # equation = equation.apply(abs)
         equation = equation.astype('float64')
-        print("equation")
-        print(equation)
         min_coef_index = equation.argmin()
 
         return {
@@ -47,7 +47,7 @@ class Simplex():
 
     def __transform_pivot_row(self, iteration: pd.DataFrame, pivot: dict) -> pd.DataFrame:
         iteration["operations"][pivot["position"]
-                                ["row"]] = f'LP <- (1/{pivot["value"]})*LP'
+                                ["row"]] = f'LP<-(1/{pivot["value"]})*LP'
         for j in range(0, iteration.shape[1] - 2):
             iteration.iloc[pivot["position"]["row"],
                            j] = iteration.iloc[pivot["position"]["row"], j] / pivot["value"]
@@ -68,10 +68,8 @@ class Simplex():
                     iteration.iloc[i, j] = iteration.iloc[i,
                                                           j] + tmp_pivot_row[j]
 
-                iteration["operations"][i] = f'L{i} <- L{i} + ({pivot_row_coef *-1}*LP)'
+                iteration["operations"][i] = f'L{i}<-L{i}+({pivot_row_coef *-1}*LP)'
 
-        print("===============================")
-        print(iteration)
         return iteration
 
     def __reset_operations(self, iteration: pd.DataFrame) -> pd.DataFrame:
@@ -89,18 +87,43 @@ class Simplex():
 
         return done
 
+    def __find_variable_by_equation_index(self, associated_equation: int, variables: Variables) -> Variable | None:
+        target_variable: Variable = None
+        for variable in variables:
+            if variable[2] == associated_equation:
+                target_variable = variable
+                break
+
+        return target_variable
+
+    def __find_variable_by_name(self, name: str, variables: Variables) -> Variable:
+        target_variable: Variable = None
+        for variable in variables:
+            if variable[0] == name:
+                target_variable = variable
+                break
+
+        return target_variable
+
+    def __update(self, iteration: pd.DataFrame, basic_variables: Variables) -> Variables:
+        updated_basic_variables = []
+        for variable in basic_variables:
+            updated_basic_variables.append(
+                (variable[0], iteration['condition'][variable[2]], variable[2]))
+        return updated_basic_variables
+
     def _perform_simplex(self, init_simplex_df: pd.DataFrame) -> Iterations:
-        # variables: Variables = self.__select_vars(init_simplex_df)
+        # variables = self.__select_vars(init_simplex_df)
         # exist = self.__verify_solution_existence(variables)
         # if not exist:
         #     raise ValueError('SIMPLEX_SOLULTION_DOES_NOT_EXIST,')
 
-        # init_iteration = {
-        #     "matrix": DataFrame, "variables": {"bv": [{"name": "x1": "value": 3.0}, {"name": "x2": "value": 2.0}], "nbv": [{"name": "s1": "value": 0}, {"name": "s2": "value": 0}]}
-        #     }
-
         # Test
         init_simplex_df = pd.read_csv("simplex_data.csv")
+        bv = [("s1", 20, 0), ("a2", 5, 1),
+              ("a3", 10, 2), ("p", -15000000000, 3)]
+        nbv = [("x1", 0, -1), ("x2", 0, -1), ("x3", 0, -1), ("s3", 0, -1)]
+        # End Test
 
         iteration = init_simplex_df.copy()
         ratio = [0] * iteration.shape[0]
@@ -108,6 +131,7 @@ class Simplex():
         iteration['ratio'] = ratio
         iteration['operations'] = operations
         iterations: Iterations = []
+        iterations.append((iteration.copy(), bv, nbv))
         done = False
         while not done:
             # Determine the max coef position
@@ -147,9 +171,24 @@ class Simplex():
             done = self.__verify(iteration)
 
             # Update basic and non basic variables and the iterations list
-            # ...
+            bv = self.__update(iteration, bv)
+
+            # Finding the variable that is going to leave the basic variables
+            out_variable = self.__find_variable_by_equation_index(
+                pivot["position"]["row"], bv)
+
+            # Creation of the variable that is going to be added to the basic variables
+            in_variable = (iteration.columns[pivot["position"]["col"]],
+                           iteration["condition"][out_variable[2]], out_variable[2])
+
+            bv.remove(out_variable)
+            nbv.append((out_variable[0], 0, -1))
+
+            nbv.remove(self.__find_variable_by_name(in_variable[0], nbv))
+            bv.append(in_variable)
+
+            iterations.append((iteration.copy(), bv, nbv))
 
             iteration = self.__reset_operations(iteration)
-            input('type something to coninue ...')
 
         return iterations
