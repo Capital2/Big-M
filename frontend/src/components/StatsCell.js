6eee7a7231b51a3805ea8fb13f360e8e11fa5be1
BlueGob <moslemgannoun@gmail.com>
2022-11-20 15:39:43 +0100

    add props

--- frontend/src/components/StatsCell.js
+++ frontend/src/components/StatsCell.js
@@ -4,11 +4,11 @@ export default function StatCell(props) {
   return (
 
        <div className="d-flex flex-column mb-5">
-        <div className="p-2">Flex item 1</div>
+        <div className="p-2">{props.title}</div>
         <div className="p-2">
           <div class="d-flex justify-content-around">
-            <div class="p-3">    <h1 >5</h1></div>
-            <div class="p-3"> <MDBIcon fas icon="redo" size='4x' /></div>
+            <div class="p-3"><h1 >{props.value}</h1></div>
+            <div class="p-3"> <MDBIcon fas icon={props.icon} size='4x' /></div>
           </div>
         </div>
       </div>
--- frontend/src/components/StatsCellList.js
+++ frontend/src/components/StatsCellList.js
@@ -3,11 +3,13 @@ import React from 'react'
 import StatCell from './StatsCell';
 export default function StatCellList(props) {
   return (
+    /*icons to choose from: https://mdbootstrap.com/docs/react/content-styles/icons/ */
     <div class="d-flex justify-content-around">
-        <div class="p-3"> <StatCell/></div>
-        <div class="p-3"> <StatCell/></div>
-        <div class="p-3"> <StatCell/></div>
-        <div class="p-3"> <StatCell/></div>
+        
+        <div class="p-3"> <StatCell title="Iteration" value={5} icon="redo"/></div>
+        <div class="p-3"> <StatCell title="Time" value={0.5} icon ="clock"/></div>
+        <div class="p-3"> <StatCell title="Gain" value={1800} icon="chart-line"/></div>
+        <div class="p-3"> <StatCell title="x= y ="value={50} icon ="map-pin"/></div>
     </div>
    
 
