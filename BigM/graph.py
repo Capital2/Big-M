fbf8b1b4bd23d805c43ee4451a8b9540fccb31be
achrafoo2018 <achrafb.s2015@gmail.com>
2022-11-22 14:27:14 +0100

    fixed bug when 2 constraints are parallel

--- BigM/graph.py
+++ BigM/graph.py
@@ -104,11 +104,13 @@ def drawGraph(formattedUserInput):
                 a2 = constraints[j][0]
                 b2 = constraints[j][1]
                 c2 = constraints[j][2]
-
-                x = (b2*c1 - b1*c2)/(a1*b2 - a2*b1) 
-                y = (a1*c2 - a2*c1)/(a1*b2 - a2*b1)
-                if x >= 0 and y >= 0:
-                    intersections.add((x,y))
+                try:
+                    x = (b2*c1 - b1*c2)/(a1*b2 - a2*b1) 
+                    y = (a1*c2 - a2*c1)/(a1*b2 - a2*b1)
+                    if x >= 0 and y >= 0:
+                        intersections.add((x,y))
+                except ZeroDivisionError: # parallel lines
+                    pass
         for intersection in intersections:
             # check if intersection is in the feasible region            
             if not inFeasibleRegion(constraints, intersection):
